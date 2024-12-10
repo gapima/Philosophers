2dd87bb163567b37a96012a883633772b5ef1368
Gabriel Lima <gabriel.moura@vertem.com>
2024-12-10 08:27:38 -0300

    /home/gab

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -1,7 +1,6 @@
-#ifndef PHILO_H
+#ifndef	PHILO_H
 # define PHILO_H
 
-// #include <bits/pthreadtypes.h>
 # include <pthread.h>
 # include <limits.h>
 # include <unistd.h>
@@ -13,49 +12,46 @@
 
 typedef struct s_table
 {
-	int 			number_of_philosophers;
-	int 			time_to_die;
-	int 			time_to_eat;
-	int 			time_to_sleep;
+	int				number_of_philosophers;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
 	int				number_of_times_each_philosopher_must_eat;
 	pthread_t		verify_each_eat;
 	int				quantity_have_philo;
 	pthread_mutex_t	how_philo_eat;
-	pthread_mutex_t *all_fork;
+	pthread_mutex_t	*all_fork;
 	bool			simulation_running;
 	unsigned long	time_start;
 	pthread_mutex_t	write_action;
-} t_table;
+}	t_table;
 
 typedef struct s_philo
 {
 	pthread_t		thread_id;
 	int				id;
-	pthread_mutex_t *fork_left;
-	pthread_mutex_t *fork_right;
+	pthread_mutex_t	*fork_left;
+	pthread_mutex_t	*fork_right;
 	int				quantity_eat;
 	pthread_mutex_t	eat_now;
 	long			eat_last_time;
 	t_table			*table;
-} t_philo;
+}	t_philo;
 
 typedef enum e_action
 {
 	THINKING,
 	EATING,
-    SLEEPING,
-    DIED,
+	SLEEPING,
+	DIED,
 	HUNGRY
-} t_action;
+}	t_action;
 
-void    *routine(void *data);
-long ft_get_time();
-void    print_action(t_action action, t_philo *philo);
-void sleep_routine(int time_sleep);
-void    leave_forks(t_philo *philo);
-void    get_forks(t_philo *philo);
-void   eat_routine(t_philo *philo);
-
-
-
+void	*routine(void *data);
+long	ft_get_time();
+void	print_action(t_action action, t_philo *philo);
+void	sleep_routine(int time_sleep);
+void	leave_forks(t_philo *philo);
+void	get_forks(t_philo *philo);
+void	eat_routine(t_philo *philo);
 #endif
--- philo/src/main.c
+++ philo/src/main.c
@@ -1,12 +1,9 @@
 #include "philo.h"
-#include <bits/pthreadtypes.h>
-#include <pthread.h>
-#include <unistd.h>
 
-static void ft_destroy(t_philo *philo)
+static void	ft_destroy(t_philo *philo)
 {
 	int			count;
-	t_table 	*table;
+	t_table		*table;
 
 	count = 0;
 	table = philo->table;
@@ -15,16 +12,16 @@ static void ft_destroy(t_philo *philo)
 	{
 		pthread_mutex_destroy(&philo[count].eat_now);
 		pthread_mutex_destroy(&table->all_fork[count]);
-		count++;	
+		count++;
 	}
 	free(philo);
 	free(table->all_fork);
 }
 
-static void ft_delegateFork(t_philo *philo)
+static	void ft_delegateFork(t_philo *philo)
 {
-	int			count;
-	t_table 	*table;
+	int		count;
+	t_table	*table;
 
 	count = 0;
 	table = philo->table;
@@ -33,26 +30,29 @@ static void ft_delegateFork(t_philo *philo)
 		if ((count % 2) == 0)
 		{
 			philo[count].fork_left = &table->all_fork[count];
-			philo[count].fork_right = &table->all_fork[(count + 1) % table->number_of_philosophers];
+			philo[count].fork_right = &table->all_fork[(count + 1) % \
+			table->number_of_philosophers];
 		}
 		else
 		{
 			philo[count].fork_right = &table->all_fork[count];
-			philo[count].fork_left = &table->all_fork[(count + 1) % table->number_of_philosophers];
+			philo[count].fork_left = &table->all_fork[(count + 1) % \
+			table->number_of_philosophers];
 		}
-		count++;	
+		count++;
 	}
 }
 
 void	*verify_philo_eat(void *data)
 {
-	t_table *table;
+	t_table	*table;
 
 	table = data;
 	while (table->simulation_running)
 	{
 		pthread_mutex_lock(&table->how_philo_eat);
-		if (table->quantity_have_philo >= table->number_of_times_each_philosopher_must_eat)
+		if (table->quantity_have_philo \
+		>= table->number_of_times_each_philosopher_must_eat)
 			table->simulation_running = false;
 		pthread_mutex_unlock(&table->how_philo_eat);
 		sleep_routine(10);
@@ -60,11 +60,10 @@ void	*verify_philo_eat(void *data)
 	return (NULL);
 }
 
-
 int main(int ac, char **av)
 {
-	int count;
-	t_table table;
+	int		count;
+	t_table	table;
 	t_philo	*all_philo;
 
 	table.number_of_times_each_philosopher_must_eat = -1;
@@ -72,7 +71,7 @@ int main(int ac, char **av)
 	if (ac < 5)
 	{
 		ft_putstr_fd("Deu ruim0\n", 1);
-		return 1;
+		return (1);
 	}
 	table.number_of_philosophers = ft_atoi(av[1]);
 	table.time_to_die = ft_atoi(av[2]);
@@ -88,10 +87,11 @@ int main(int ac, char **av)
 		|| table.time_to_eat <= 0 || table.time_to_sleep <= 0)
 	{
 		ft_putstr_fd("\nDeu ruim1\n", 1);
-		return 1;
+		return (1);
 	}
-	all_philo = ft_calloc(table.number_of_philosophers,sizeof(t_philo));
-	table.all_fork = ft_calloc(table.number_of_philosophers,sizeof(pthread_mutex_t));
+	all_philo = ft_calloc(table.number_of_philosophers, sizeof(t_philo));
+	table.all_fork = ft_calloc(table.number_of_philosophers \
+	sizeof(pthread_mutex_t));
 	count = 0;
 	pthread_mutex_init(&table.write_action, 0);
 	while (count < table.number_of_philosophers)
@@ -100,10 +100,9 @@ int main(int ac, char **av)
 		all_philo[count].table = &table;
 		pthread_mutex_init(&all_philo[count].eat_now, 0);
 		if (pthread_mutex_init(&table.all_fork[count], 0) < 0)
-			return 1;
+			return (1);
 		count++;
 	}
-
 	if (table.number_of_philosophers == 1)
 	{
 		table.time_start = ft_get_time();
@@ -136,5 +135,5 @@ int main(int ac, char **av)
 			return (1);
 	pthread_mutex_destroy(&table.how_philo_eat);
 	ft_destroy(all_philo);
-	return 0;
+	return (0);
 }
